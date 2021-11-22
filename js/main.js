eef931e78f0ab55d14575400939754be1efbae35
Grant Gillespie <gillegra@gmail.com>
2021-11-22 00:35:04 -0600

    Add createSelectOptions function

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ const createElemWithText = (newElement = "p", newTextContent = "", newClassName 
   return newThing;
 }
 
+const createSelectOptions = (userJsonData = null) => {
+  if (!userJsonData) {
+    return undefined;
+  }
+  if (Array.isArray(userJsonData) === false) {
+    userJsonData = [userJsonData];
+  }
+  
+  const optionsArr = [];
+  for (let i = 0; i < userJsonData.length; i++) {
+    const user = userJsonData[i];
+    const optionElement = document.createElement("option");
+    optionElement.value = user.id;
+    optionElement.textContent = user.name;
+    optionsArr.push(optionElement);
+  };
+
+  return optionsArr;
+}
