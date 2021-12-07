2d7033b0a65a533fc89eb80ac5948f788dbf1011
Grant Gillespie <gillegra@gmail.com>
2021-12-07 00:23:36 -0600

    Fix refreshPosts

--- js/main.js
+++ js/main.js
@@ -549,19 +549,12 @@ const refreshPosts = async (posts) => {
   }
 
   const resultRemoveButtonListeners = removeButtonListeners();
-  const resultDeleteChildElements = deleteChildElements();
+  const resultDeleteChildElements = deleteChildElements(document.querySelector("main"));
   const resultDisplayPosts = await displayPosts(posts);
   const resultAddButtonListeners = addButtonListeners();
 
   return [resultRemoveButtonListeners, resultDeleteChildElements, resultDisplayPosts, resultAddButtonListeners];
 }
-/*
-TODO fix final failing test:
-The function refreshPosts should return an array of results with accurate data.AssertionError: expected 3 to equal 2
-    at n.<anonymous> (https://serene-roentgen-345cd7.netlify.app/tests/finalProject.min.js:18:45880)
-    line 1182 in prettified
-    assert.strictEqual(result[3].length, 2)
-*/
 
 
 /**
